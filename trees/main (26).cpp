e35a0c252834d34d9e7d1aac78265f87e1e169dc
Rajat2710 <44227923+Rajat2710@users.noreply.github.com>
2020-02-19 22:46:26 +0530

    mirror image of bst

---
+++ trees/main (26).cpp
@@ -0,0 +1,80 @@
+
+#include <iostream>
+#include<stack>
+#include<queue>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *left;
+    Node *right;
+};
+Node *newNode(int val){
+    Node *node=new Node;
+    node->data=val;
+    node->left=node->right=NULL;
+    return node;
+}
+Node *insertlevelorder(int *arr,Node *root,int i,int n){
+    if(i<n){
+        Node *temp=newNode(arr[i]);
+        root=temp;
+        root->left=insertlevelorder(arr,root->left,2*i+1,n);
+        root->right=insertlevelorder(arr,root->right,2*i+2,n);
+    }
+    return root;
+}
+
+void levelorder(Node *root){
+    if(root==NULL)
+    return;
+    Node *curr=root;
+    queue<Node *>q;
+    q.push(curr);
+    while(!q.empty()){
+        Node *node=q.front();
+        cout<<node->data<<" ";
+        q.pop();
+        if(node->left!=NULL)
+        q.push(node->left);
+        if(node->right!=NULL)
+        q.push(node->right);
+    }
+    cout<<endl;
+}
+Node *sortedArrayToBST(int *arr,int l, int r){
+    if(l>r)
+    return NULL;
+    int mid=(l+r)/2;
+    Node *root=newNode(arr[mid]);
+    root->left=sortedArrayToBST(arr, l, mid-1);
+    root->right=sortedArrayToBST(arr, mid+1, r);
+    return root;
+}
+void mirror(Node *root){
+    if(root==NULL)
+    return;
+    else{
+        mirror(root->left);
+        mirror(root->right);
+        
+        Node *temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+    }
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};  
+    int n = sizeof(arr) / sizeof(arr[0]);  
+  
+    Node *root = insertlevelorder(arr,root,0, n);  
+    cout << "levelOrder Traversal of constructed BST \n";  
+    levelorder(root);
+     mirror(root);
+     cout<<"After mirror image"<<endl;
+  cout << "levelOrder Traversal of constructed BST \n";  
+    levelorder(root); 
+    return 0; 
+}
